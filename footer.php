f094ac9ddf4c6f39dc2560ba90f393e7121a9ddb
marion2a <marion.amezquita@edu.ece.fr>
2022-03-19 18:28:34 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="page-footer">   
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-md-8 col-sm-12">
+					<h6 class="text-uppercase font-weight-bold">Information additionnelle</h6>
+					<p>
+						Merci d'avoir visité notre site, à bientôt !
+					</p>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+					<h6 class="text-uppercase font-weight-bold">Contact</h6>
+					<p>GreenMarket<br>
+			<a href="mailto:marketplace@ece.fr">Envoyez-nous un mail</a><br>
+			Appelez-nous au : 01 02 03 04 05<br>
+			Envoyez-nous une lettre au: 37 Quai de Grenelle, 75015 Paris<br>
+			<p>
+			</div>
+		</div>
+		<div class="footer-copyright text-center">&copy; 2022 Copyright | Droit d'auteur
+		</div>
+	</footer>
+</body>
+</html>
